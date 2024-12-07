011a9695dc49b097c0b5db39a01e5fd0f31ef2a9
Matias Heusdens <heusdensmatias@gmail.com>
2024-12-07 13:15:26 -0300

    remove

--- index.js
+++ index.js
@@ -1,43 +1,70 @@
-
+const ingresos = [];
 const gastos = [];
 
-function AgregarIngreso(ingreso, concepto){
+function AgregarIngreso(ingreso, concepto) {
     ingresos.push({
-        ingreso, 
+        ingreso,
         concepto
-    })
+    });
 }
 
-function core(){
-    let bandera = true
+function core() {
+    let bandera = true;
 
-    while(bandera){
-        let opciones = Number(prompt("Eloge una opción:\n 1-Ingreso\n 2-Gasto\n 3-Eliminar Ingreso\n 4-Eliminar Gasto\n 5-Mostrar Ingresos y Gastos\n "))
-    
-        switch(opciones){
-            case 0:
-                return
+    while (bandera) {
+        let opciones = Number(
+            prompt(
+                "Elige una opción:\n 1-Ingreso\n 2-Gasto\n 3-Mostrar Ingresos y Gastos\n 4-Salir"
+            )
+        );
+
+        switch (opciones) {
             case 1:
-                let ingreso = Number(prompt("Importe de ingreso:"))
-                let concepto = prompt("Concepto del ingreso")
-                console.log(concepto + ingreso)
-                AgregarIngreso()
-                bandera = confirm("Continuar...")
-                break
+                let ingreso = Number(prompt("Importe de ingreso:"));
+                if (!isNaN(ingreso) && ingreso > 0) {
+                    let concepto = prompt("Concepto del ingreso:");
+                    AgregarIngreso(ingreso, concepto);
+                    alert("Ingreso registrado.");
+                } else {
+                    alert("Por favor, ingresa un importe válido.");
+                }
+                break;
+
             case 2:
-                bandera = confirm("Continuar...")
-                break
+                let gasto = Number(prompt("Importe del gasto:"));
+                if (!isNaN(gasto) && gasto > 0) {
+                    let descripcion = prompt("Descripción del gasto:");
+                    gastos.push({
+                        gasto,
+                        descripcion
+                    });
+                    alert("Gasto registrado.");
+                } else {
+                    alert("Por favor, ingresa un importe válido.");
+                }
+                break;
+
             case 3:
-                bandera = confirm("Continuar...")
-                break
+                console.log("=== Ingresos ===");
+                ingresos.forEach((ing, index) =>
+                    console.log(`${index + 1}. ${ing.concepto}: $${ing.ingreso}`)
+                );
+                console.log("=== Gastos ===");
+                gastos.forEach((g, index) =>
+                    console.log(`${index + 1}. ${g.descripcion}: $${g.gasto}`)
+                );
+                break;
+
             case 4:
-                bandera = confirm("Continuar...")
-                break
-            case 5:
-                bandera = confirm("Continuar...")
-                break
+                bandera = false;
+                alert("Saliendo del programa.");
+                break;
+
+            default:
+                alert("Opción no válida. Inténtalo nuevamente.");
+                break;
         }
     }
 }
 
-core()
+core();
